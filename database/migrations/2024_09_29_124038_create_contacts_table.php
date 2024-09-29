29068cbf18a5da40252ae398085a6a74c48d7703
Shivani Indral <shivani.indral@gmail.com>
2024-09-29 18:19:21 +0530

    Created Controller

--- database/migrations/2024_09_29_124038_create_contacts_table.php
+++ database/migrations/2024_09_29_124038_create_contacts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('phone');
             $table->timestamps();
         });
     }
