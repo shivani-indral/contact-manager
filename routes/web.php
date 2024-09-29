0d653112133fa4a7b358c5bec76d645f80519d54
Shivani Indral <shivani.indral@gmail.com>
2024-09-29 19:18:32 +0530

    Created route for importing XMl

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use App\Http\Controllers\ContactController;
 
 Route::resource('contacts', ContactController::class);
+Route::post('contacts/import', [ContactController::class, 'importXML'])->name('contacts.import');
